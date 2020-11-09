8ddc3b3b07182ec84efc5bfc362ba995141b8356
Viacheslav <evteevne.ss@gmail.com>
2020-11-09 23:32:58 +0300

    Продолжение изучения C#

---
+++ Chapter_11_AdvancedToolsCS/SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace SimpleIndexer
+{
+    public class PersonCollection : IEnumerable
+    {
+        private readonly ArrayList _arPeople = new ArrayList();
+
+        // Cast for caller.
+        public Person GetPerson(int pos) => (Person)_arPeople[pos];
+
+        public Person this[int index]
+        {
+            get => (Person) _arPeople[index];
+            set => _arPeople.Insert(index, value);
+        }
+
+
+        // Only insert Person types.
+        public void AddPerson(Person p)
+        { _arPeople.Add(p); }
+
+        public void ClearPeople()
+        { _arPeople.Clear(); }
+
+        public int Count => _arPeople.Count;
+
+        // Foreach enumeration support.
+        IEnumerator IEnumerable.GetEnumerator() => _arPeople.GetEnumerator();
+    }
+
+}
---
+++ Chapter_11_AdvancedToolsCS/SimpleIndexer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleIndexer
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Пример создания индексатора";
+            Console.ForegroundColor = ConsoleColor.Green;
+            var people = new PersonCollection();
+            people[0] = new Person("Artem", "Muratov", 30);
+            people[1] = new Person("Gena", "Acrabatov", 27);
+            people[2] = new Person("Leonid", "Filatov", 50);
+            people[3] = new Person("Oleg", "Penetratov", 18);
+            for (var i = 0; i < people.Count; i++)
+            {
+                Console.Write($"Номер лица: {i} ");
+                Console.WriteLine($"Фамилия: {people[i].LastName}, Имя: {people[i].FirstName}, Возраст: {people[i].Age}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
