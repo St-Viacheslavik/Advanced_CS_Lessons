54a5b55d1250cf5da8e82b071d58723de9170396
Viacheslav <evteevne.ss@gmail.com>
2021-03-02 21:16:20 +0300

    начата глава 13

---
+++ Chapter_13_AliveTimeObjects/SimpleGC/Car.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleGC
+{
+    public class Car
+    {
+        public string CarName { get; set; }
+        public int CarSpeed { get; set; }
+
+        public Car() : this("NaN", 0) { }
+        public Car(string carName, int carSpeed)
+        {
+            CarName = carName;
+            CarSpeed = carSpeed;
+        }
+
+        [SuppressMessage("ReSharper", "CommentTypo")]
+        private static void MakeCar()
+        {
+            /*
+             * Этот метод нужен только для того чтобы посмотреть CIL код
+             */
+            var car = new Car();
+            car = null;
+        }
+
+        public override string ToString() => $"Car name: {CarName}, car speed: {CarSpeed}";
+    }
+}
---
+++ Chapter_13_AliveTimeObjects/SimpleGC/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleGC
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Простой пример сборщика мусора";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Оценочное количество байт зарезервированых в куче: {GC.GetTotalMemory(false)},\n" +
+                              $"в килобайтах {GC.GetTotalMemory(false)/1024},\n" +
+                              $"в мегабайтах {GC.GetTotalMemory(false) / 1024/1024}");
+            Console.WriteLine($"Максимальное количество поколений: {GC.MaxGeneration + 1}");
+            var car = new Car("Honda", 175);
+            Console.WriteLine(car.ToString());
+            Console.WriteLine();
+            Console.WriteLine($"Поколоение этого объекта: {GC.GetGeneration(car)}");
+            var moreCars = new Car [50000];
+            for (var i = 0; i < moreCars.Length; i++)
+            {
+                moreCars[i] = new Car();
+            }
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+            Console.WriteLine(moreCars[2000] != null
+                ? $"Поколоение объекта moreCars[2000]: {GC.GetGeneration(car)}"
+                : $"Объект moreCars[2000] больше не существует");
+            Console.WriteLine($"Поколоение 0 изменилось {GC.CollectionCount(0)} раз");
+            Console.WriteLine($"Поколоение 1 изменилось {GC.CollectionCount(1)} раз");
+            Console.WriteLine($"Поколоение 2 изменилось {GC.CollectionCount(2)} раз");
+            Console.ReadLine();
+        }
+    }
+}
