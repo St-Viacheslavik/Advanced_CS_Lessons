e122e5f950c0258c75869f3a5a61bac275d34725
Viacheslav <evteevne.ss@gmail.com>
2021-06-03 15:41:17 +0300

    Начата глава 17

---
+++ Chapter_17_ProcessesDomens/ProcessManipulator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace ProcessManipulator
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Работа с процессами";
+            Console.ForegroundColor = ConsoleColor.Green;
+            ListAllRunningProcess();
+            GetSpecificProcess();
+            Console.ReadLine();
+        }
+
+        private static void GetSpecificProcess()
+        {
+            Console.WriteLine("Получить процесс по оперделенному PID или вывести ошибку");
+            Console.Write("Введите PID = ");
+            var pid = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            try
+            {
+                var proc = Process.GetProcessById(pid);
+                Console.WriteLine($"Process name: {proc.ProcessName}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void ListAllRunningProcess()
+        {
+            Console.WriteLine("Список всех процессов на локальном компьютере");
+            var processes = Process.GetProcesses(".").OrderBy(proc => proc.Id).Select(proc => proc);
+            Console.WriteLine("***********************************************");
+            foreach (var process in processes)
+            {
+                Console.WriteLine($"\tPID = {process.Id},\t Name = {process.ProcessName}");
+            }
+            Console.WriteLine("***********************************************");
+        }
+    }
+}
