55bc5109c27bcf5eec651814659cb0a2c74412d9
Viacheslav <evteevne.ss@gmail.com>
2021-06-03 17:40:25 +0300

    Приложение по созданию собственных доменов

---
+++ Chapter_17_ProcessesDomens/CustomAppDomain/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace CustomAppDomain
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Создание собственных доменов";
+            Console.ForegroundColor = ConsoleColor.Green;
+            var defaultAd = AppDomain.CurrentDomain;
+            defaultAd.ProcessExit += (sender, args) => { Console.WriteLine("Базовый домен был выгружен"); };
+            DisplayAssemblies(defaultAd);
+            MakeNewAppDomain();
+            Console.ReadLine();
+        }
+
+        private static void MakeNewAppDomain()
+        {
+            var domain = AppDomain.CreateDomain("My New Domain");
+            domain.DomainUnload += (o, s) => { Console.WriteLine("Домен был выгружен"); };
+            try
+            {
+                domain.Load("CarLibrary");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            DisplayAssemblies(domain);
+            AppDomain.Unload(domain);
+        }
+
+        private static void DisplayAssemblies(AppDomain defaultAd)
+        {
+            Console.WriteLine("*************************************************************************");
+            var assemblies = defaultAd.GetAssemblies().OrderBy(name => name.FullName);
+            Console.WriteLine($"Все сборки внутри домена {defaultAd.FriendlyName}");
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine($"-> {assembly.GetName().Name}");
+                Console.WriteLine($"-> {assembly.GetName().Version}\n");
+            }
+            Console.WriteLine("*************************************************************************");
+        }
+    }
+}
