c5a4effd178fcf6ec83d2178d7e5de096fc3a68c
Viacheslav <evteevne.ss@gmail.com>
2021-06-03 17:52:02 +0300

    Закончена глава 17

---
+++ Chapter_17_ProcessesDomens/ObjectContextApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ObjectContextApp
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Исследование контекста объекта";
+            Console.ForegroundColor = ConsoleColor.Green;
+            var sportCar = new SportCar();
+            Console.WriteLine();
+            var sportCar2 = new SportCar();
+            Console.WriteLine();
+            var sportCarTs = new SportCarTs();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter_17_ProcessesDomens/ObjectContextApp/SportCarTS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
+
+namespace ObjectContextApp
+{
+    public class SportCar
+    {
+        public SportCar()
+        {
+            var context = Thread.CurrentContext;
+            Console.WriteLine($"{this} object in context {context.ContextID}");
+            if (context.ContextProperties == null) return;
+            foreach (var contextContextProperty in context.ContextProperties)
+            {
+                Console.WriteLine($"context prop {contextContextProperty.Name}");
+            }
+        }
+    }
+
+    [Synchronization]
+    public class SportCarTs : ContextBoundObject
+    {
+        public SportCarTs()
+        {
+            var context = Thread.CurrentContext;
+            Console.WriteLine($"{this} object in context {context.ContextID}");
+            if (context.ContextProperties == null) return;
+            foreach (var contextContextProperty in context.ContextProperties)
+            {
+                Console.WriteLine($"context prop {contextContextProperty.Name}");
+            }
+        }
+    }
+}
