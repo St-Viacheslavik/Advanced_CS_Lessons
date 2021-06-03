d1c17d95a292ee2fc260075dc81023e1d952b8d8
Viacheslav <evteevne.ss@gmail.com>
2021-06-03 17:14:33 +0300

    Добавлено приложение с просмотром базового домена

---
+++ Chapter_17_ProcessesDomens/DefaultAppDomain/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace DefaultAppDomain
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Использование класса AppDomain";
+            Console.ForegroundColor = ConsoleColor.Green;
+            InitDad();
+            DisplayStat();
+            DisplayAssemblies();
+            Console.ReadLine();
+        }
+
+        private static void InitDad()
+        {
+            var defaultAd = AppDomain.CurrentDomain;
+            defaultAd.AssemblyLoad += DefaultAd_AssemblyLoad;
+        }
+
+        private static void DefaultAd_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
+        {
+            Console.WriteLine($"Сборка {args.LoadedAssembly.GetName().Name} была загружена");
+        }
+
+        private static void DisplayAssemblies()
+        {
+            Console.WriteLine("*************************************************************************");
+            var defaultAd = AppDomain.CurrentDomain;
+            var assemblies = defaultAd.GetAssemblies().OrderBy(name => name.FullName);
+            Console.WriteLine($"Все сборки внутри домена {defaultAd.FriendlyName}");
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine($"-> {assembly.GetName().Name}");
+                Console.WriteLine($"-> {assembly.GetName().Version}\n");
+            }
+            Console.WriteLine("*************************************************************************");
+        }
+
+        private static void DisplayStat()
+        {
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Base Display");
+            var defaultAd = AppDomain.CurrentDomain;
+            Console.WriteLine($"Name: {defaultAd.FriendlyName},\tID = {defaultAd.Id},\tIdBaseDomain = {defaultAd.IsDefaultAppDomain()}");
+            Console.WriteLine($"LocalPath {defaultAd.BaseDirectory}");
+            Console.WriteLine("*************************************************************************");
+        }
+    }
+}
